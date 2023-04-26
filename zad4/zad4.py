a430ef9794142a007a29732e10396a1aabe93394
JakubFr4czek <jakubfraczek.m8@gmail.com>
2023-04-26 21:42:18 +0200

    haaland haaland yorkshire norwegian lad

--- zad4/zad4.py
+++ zad4/zad4.py
@@ -6,7 +6,6 @@ def bfs(G, poczatek, koniec, banned1, banned2):
     visited = [False for _ in range(len(G))]
 
     queue = deque()
-    sciezka = deque()
     skad = [ -1 for _ in range(len(G))]
 
     queue.append(poczatek)
@@ -15,7 +14,6 @@ def bfs(G, poczatek, koniec, banned1, banned2):
     while len(queue) > 0:
          
         temp = queue.popleft()
-        sciezka.append(temp)
         if temp == koniec:
             break
 
@@ -27,26 +25,22 @@ def bfs(G, poczatek, koniec, banned1, banned2):
                     queue.append(G[temp][i])
                     skad[G[temp][i]] = temp
                     visited[G[temp][i]] = True
+                    
 
-    print("skad: ",skad)
-
-    if sciezka[len(sciezka) - 1] != koniec: #sciezki nie znaleziono
+    if skad[koniec] == -1:
         return []
-
-    #ekstrakcja sciezki (fajnie brzmi)
 
     res = deque()
 
-    if len(sciezka) %2 == 1:
-        res.append(sciezka[0])
-        #print(len(sciezka)//2)
-        for i in range(2, len(sciezka), 2):
-            res.append(sciezka[i])
-    else:
-        res.append(sciezka[0])
-        for i in range(1, len(sciezka), 2):
-            res.append(sciezka[i])
-    
+    pos = koniec
+
+    while pos != poczatek:
+
+        res.append(pos)
+
+        pos = skad[pos]
+    res.append(poczatek)
+    #print(res)
     return res
 
     #if len(sciezka) %2 == 1:
@@ -59,33 +53,40 @@ def longer(G, poczatek, koniec):
     sciezka = deque()
 
     sciezka = bfs(G, poczatek, koniec ,-1 ,-1) #najkrotsza sciezka (tak mi sie wydaje)
+
+
+    #print(sciezka)
+
     if len(sciezka) == 0:
-        return 0
+        return None
 
     #print(sciezka)
 
     res = bfs(G, poczatek, koniec, sciezka[0], sciezka[1])
-    print(res)
+    
     res2 = (sciezka[0], sciezka[1])
 
     for i in range(2, len(sciezka)):
 
         temp = bfs(G, poczatek, koniec, sciezka[i - 1], sciezka[i])
-
-        if len(temp) < len(res):
+        #print(sciezka[i - 1], sciezka[i])
+        if len(temp) == 0: return (sciezka[i - 1], sciezka[i])
+        if len(temp) > len(res):
             res = temp
             res2 = (sciezka[i - 1], sciezka[i])
 
-    if len(temp) > len(sciezka):
+    if len(res) > len(sciezka):
+        return res2
+    elif len(res) == 0:
         return res2
     else:
-        return 0
+        return None
     
 
 
-G = [[4, 6], [2,6], [1, 3], [2, 5], [0, 5], [4, 3], [0,1]]
+#G = [[4, 6], [2,6], [1, 3], [2, 5], [0, 5], [4, 3], [0,1]]
 
-print(longer(G, 0, 2))
+#print(longer(G, 0, 2))
 
 # zmien all_tests na True zeby uruchomic wszystkie testy
-#runtests( longer, all_tests = False )
+runtests( longer, all_tests = True )
